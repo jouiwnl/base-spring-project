937ad8f7172c4708df0867745138fa139040e32e
jouiwnl <jouiwnl>
2022-05-08 22:02:23 -0300

    adding a basicrepository with generic methods for any class

--- src/main/java/com/joao/sampleproject/core/DynamicDto.java
+++ src/main/java/com/joao/sampleproject/core/DynamicDto.java
@@ -1,16 +1,13 @@
 package com.joao.sampleproject.core;
 
-import org.apache.commons.beanutils.BeanUtils;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.Objects;
 
 public class DynamicDto extends LinkedHashMap<String, Object> {
-
-    private static final String DESCRIPTION_FIELD = "descricao";
 
     private DynamicDto() {
     }
@@ -24,44 +21,34 @@ public class DynamicDto extends LinkedHashMap<String, Object> {
     }
 
     public static DynamicDto of(DatabaseEntity<?> entity) {
-        return of(entity, (Field) null);
+        return of(entity, false);
     }
 
-    public static DynamicDto of(DatabaseEntity<?> entity, Field field) {
+    public static DynamicDto of(DatabaseEntity<?> entity, boolean autoGenerate) {
         if (entity == null) {
             return null;
         }
+        final DynamicDto dto = new DynamicDto();
+        if (Boolean.TRUE.equals(autoGenerate)) {
+            Arrays.stream(entity.getClass().getDeclaredMethods()).sorted(Comparator.comparing(Method::getName)).forEach(method -> {
+                Character firstLetterLowerCase = Character.toLowerCase(method.getName().charAt(3));
+                String restPropertyName = method.getName().substring(4, method.getName().getBytes(StandardCharsets.UTF_8).length);
+                String propertyName = firstLetterLowerCase + restPropertyName;
 
-        return new DynamicDto()
-                .with("id", entity.getId())
-                .populateField(entity, field);
-    }
-
-    public static DynamicDto of(DatabaseEntity<?> entity, Field[] fields) {
-        if (entity == null) {
-            return null;
+                try {
+                    if (method.getName().startsWith("get")) {
+                        dto.with(propertyName, method.invoke(entity));
+                    }
+                } catch(java.lang.IllegalAccessException | java.lang.reflect.InvocationTargetException ignored) {
+                    //
+                }
+            });
         }
-        final DynamicDto dto = new DynamicDto().with("id", entity.getId());
-        Arrays.stream(fields).forEach(field -> dto.populateField(entity, field));
         return dto;
     }
 
     public DynamicDto with(String key, Object value) {
         put(key, value);
-        return this;
-    }
-
-    private DynamicDto populateField(DatabaseEntity<?> entity, Field field) {
-        try {
-            String fieldName = field != null ? field.getName() : DESCRIPTION_FIELD;
-            String value = BeanUtils.getProperty(entity, fieldName);
-            if (value != null) {
-                put(fieldName, value);
-            }
-        } catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException ignored) {
-            //
-        }
-
         return this;
     }
 }
